800c4fb02b7cbe1ee6e4d50379454f9bc798714e
aligang <aligang@yandex.ru>
2017-12-25 21:06:20 +0000

    json.3

--- pprint_json.py
+++ pprint_json.py
@@ -1,9 +1,11 @@
 import json
+from sys import argv
 
 def load_data(json_filepath):
-    file_containing_json = open(json_filepath,"r")
-    dict_representing_json = json.load(file_containing_json)
+    with open(json_filepath, "r") as file_containing_json: 
+        dict_representing_json = json.load(file_containing_json)
     return dict_representing_json
+
 
 def print_pretty_json(dict_representing_json):
     string_representing_pretty_json = json.dumps(dict_representing_json,
@@ -12,8 +14,9 @@ def print_pretty_json(dict_representing_json):
                                                  separators=(',', ':'))
     print(string_representing_pretty_json)
 
+
 if __name__ == '__main__':
-    json_filepath = input("Укажите файл, где хранится некрасивый  json :")
+    json_filepath = argv[1]
+    print(json_filepath)
     dict_representing_json = load_data(json_filepath)
     print_pretty_json(dict_representing_json)
-
