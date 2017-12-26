aece909c999a5aecd2a0759be2ec2199badbc870
aligang <aligang@yandex.ru>
2017-12-26 08:11:47 +0000

    json.6

--- pprint_json.py
+++ pprint_json.py
@@ -4,20 +4,21 @@ import sys
 
 def load_data(json_filepath):
     with open(json_filepath, "r") as file_containing_json:
-        dict_representing_json = json.load(file_containing_json)
-    return dict_representing_json
+        object_representing_json = json.load(file_containing_json)
+    return object_representing_json
 
 
-def print_pretty_json(dict_representing_json):
-    string_representing_pretty_json = json.dumps(dict_representing_json,
-                                                 indent=4,
-                                                 ensure_ascii=False,
-                                                 separators=(',', ':'))
+def print_pretty_json(object_representing_json):
+    string_representing_pretty_json = json.dumps(
+        object_representing_json,
+        indent=4,
+        ensure_ascii=False,
+        separators=(',', ':')
+    )
     print(string_representing_pretty_json)
 
 
 if __name__ == '__main__':
     json_filepath = sys.argv[1]
-    print(json_filepath)
-    dict_representing_json = load_data(json_filepath)
-    print_pretty_json(dict_representing_json)
+    object_representing_json = load_data(json_filepath)
+    print_pretty_json(object_representing_json)
